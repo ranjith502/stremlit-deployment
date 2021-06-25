dcf6e89db8433a5b935935cb8c8cac6ebb4cfaf4
ranjith kumar <85454586+ranjith502@users.noreply.github.com>
2021-06-26 03:59:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,9 +32,7 @@ def main():
     if st.button("Predict"):
         result=predict_note_authentication(variance,skewness,curtosis,entropy)
     st.success('The output is {}'.format(result))
-    if st.button("About"):
-        st.text("Lets LEarn")
-        st.text("Built with Streamlit")
+    
 
 if __name__=='__main__':
     main()
