7db505cd460e7cbc6a166845e0de094f77f13556
ranjith kumar <85454586+ranjith502@users.noreply.github.com>
2021-06-26 00:26:49 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,38 @@
+
+import streamlit as st
+from PIL import Image 
+pickle_in=open('forest.pkl','rb')
+prediction=pickle.load(pickle_in)
+
+
+
+
+predictions=prediction.predict([[variance,skewness,curtosis,entropy]])
+    print(predictions)
+    return predictions
+
+
+
+def main():
+    st.title("Bank Authenticator")
+    html_temp = """
+    <div style="background-color:tomato;padding:10px">
+    <h2 style="color:white;text-align:center;">Streamlit Bank Authenticator ML App </h2>
+    </div>
+    """
+    st.markdown(html_temp,unsafe_allow_html=True)
+    variance = st.text_input("Variance","Type Here")
+    skewness = st.text_input("skewness","Type Here")
+    curtosis = st.text_input("curtosis","Type Here")
+    entropy = st.text_input("entropy","Type Here")
+    result=""
+    if st.button("Predict"):
+        result=predict_note_authentication(variance,skewness,curtosis,entropy)
+    st.success('The output is {}'.format(result))
+    if st.button("About"):
+        st.text("Lets LEarn")
+        st.text("Built with Streamlit")
+
+if __name__=='__main__':
+    main()
+    
